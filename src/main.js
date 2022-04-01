3306b3080d85bd987c66d529c1933bab36ad39a2
carevalojesus <carevalojesus@gmail.com>
2022-04-01 17:38:27 -0500

    login update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
+import {createRouter, createWebHistory} from "vue-router";
 import App from './App.vue'
 import './index.css'
+import Home from '/src/views/Home.vue'
 
-createApp(App).mount('#app')
+//create router
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    }
+  ]
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
